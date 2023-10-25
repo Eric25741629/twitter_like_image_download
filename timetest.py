b9faef55bf12d11dca3618c69f9fb0ff45bf27fe
BuildTools <unconfigured@null.spigotmc.org>
2023-10-25 10:18:37 +0800

    重新更新

--- timetest.py
+++
@@ -1,8 +0,0 @@
-#timetest
-import os,time,datetime
-start=time.time()
-for i in range(100):
-    user_data=os.getenv('APPDATA')+r'\twitter_download/'
-    fileName = user_data+datetime.date.today().strftime("%Y_%m_%d.json")
-stop=time.time()
-print(stop-start)
--- twitter_get_url.py
+++ twitter_get_url.py
@@ -121,7 +121,7 @@ class download_thread(QThread):
             self._info_box.emit('錯誤','沒有取得token 請重新輸入cookies')
             return 0
 
-    def getuserinfo(self,username,count=None):
+    def get_user_info(self,username,count=None):
         url = twitter_url.screename_url.format(username)
         userjson=requests.get(url, headers=self.headers).json()
         # print(userjson)
@@ -150,16 +150,16 @@ class download_thread(QThread):
                 return 0,media_url
             media_url.append(i[0])
         return 0,media_url
+    
     def get_url_and_cursor(self,user_id,new_cursor,media_url):
         response = requests.get(twitter_url.like_url.format(user_id,new_cursor), headers=self.headers)
         get_media_url =  list(dict.fromkeys(self.get_pic_link.findall(response.text)))
-        print(get_media_url)
+        # print(get_media_url)
         keep,media_url=self.add_url(media_url,get_media_url)
         self._progressbar.emit(-1,len(media_url))
-        #twitter_id=set.union(twitter_id,set(self.p_twt_id.findall(response.text)))
         p_cursor=new_cursor
         new_cursor =  response.json()['data']['user']['result']['timeline_v2']['timeline']['instructions'][0]['entries'][-1]['content']['value'].replace('+',"%2B").replace('/',"%2F").replace('=','')
-        print(keep,p_cursor,new_cursor)
+        # print(keep,p_cursor,new_cursor)
         return keep,p_cursor,new_cursor,media_url
 
     def error_test(self, response):
@@ -187,9 +187,9 @@ class download_thread(QThread):
             return -1
         self.get_token()
         if self.last_url=='':
-            user_id,like_count=self.getuserinfo(username=self.user_name.replace("@",''),count=1)
+            user_id,like_count=self.get_user_info(username=self.user_name.replace("@",''),count=1)
         else:
-            user_id=self.getuserinfo(username=self.user_name.replace("@",'')) #獲得userid 
+            user_id=self.get_user_info(username=self.user_name.replace("@",'')) #獲得userid 
             self.last_url = re.search("https://pbs.twimg.com/media/(.*)(?:.*|\?.*)", self.last_url.replace("?format=",'.').replace(":orig",'').split('&name')[0]).group(1)#將上次的連結提取出圖片id
             like_count=100000000
         url=twitter_url.first_like_url.format(user_id)
@@ -248,8 +248,7 @@ class download_thread(QThread):
             def get_file_names():
                 yield filepath + name
                 yield filepath + '1' + name
-            
-            # Use a for loop instead of a range loop to avoid creating an unused list
+
             for file_name in get_file_names():
                 try:
                     with open(file_name, 'wb') as file:  
@@ -261,146 +260,13 @@ class download_thread(QThread):
                     return
                 except Exception as err:
                     pass
-                    #print(err)
-
         except Exception as err:
             pass
         return
-    
-    # def download_twitter_img(self,filepath,link):
-    #     try:
-    #         for i in range(0,3):
-    #             name=link.replace("https://pbs.twimg.com/media/","").split(".")[0]
-    #             url=link
-    #             headers = { 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36',
-    #                         }
-    #             print(url)
-    #             htmlfile = requests.get(url,headers=headers,verify=False,stream=True,timeout=5)
-    #             htmlfile.raise_for_status() 
-    #             size = 0
-    #             chunk_size = 1024
-    #             if ".jpg"in url:
-    #                 name=name+".jpg"
-    #             elif ".png"in url:
-    #                 name=name+".png"
-    #             try:
-    #                 with open(filepath+name,'wb') as file: 
-    #                     for data in htmlfile.iter_content(chunk_size = chunk_size):
-    #                         file.write(data)
-    #                         size +=len(data)  
-    #                 self._progressbar.emit(1,self.urls)
-    #                 sleep(1)
-    #                 return
-    #             except:
-    #                 try:
-    #                     with open(filepath+'1'+name,'wb') as file: 
-    #                             for data in htmlfile.iter_content(chunk_size = chunk_size):
-    #                                 file.write(data)
-    #                                 size +=len(data) 
-    #                 except Exception as err:
-    #                     print(err)
-    #                     pass
-                        
-    #             break
-    #     except Exception as err:
-    #         print(err)
-    #         pass
-        
-    #     return 
 
     def run(self):
-        #import tweepy
-        # self.getcookies()
         self.new_func()
-        # if(self.state()==0):
-        #     #self._info_box.emit('錯誤','Traceback (most recent call last):  <br>File "d:/downloads/import tweepy1.py", line 1, in <module>    <br>from twitterscraper.query  <br>import query_user_info  <br>File "D:\python\envs\pkg_t\lib\site-packages\twitterscraper\__init__.py", line 13, in <module> <br>from twitterscraper.query import query_tweets  <br>File "D:\python\envs\pkg_t\lib\site-packages\twitterscraper\query.p    <br>proxies = get_proxies()  <br>File "D:\python\envs\pkg_t\lib\site-packages\twitterscraper\query.py", line 49, in get_proxies    <br>list_tr = table.find_all("tr")AttributeError: "NoneType" object has no attribute "find_all"   ')
-        #     return  
-        # download=[]
-        # bearer_token = "Bearer AAAAAAAAAAAAAAAAAAAAAEzicAEAAAAA6EZ%2BpsMWNmEaFEKsSDFqtmAU%2Bco%3DTA3yElQF4FLT2MRNd2cA70nu7xoknZ7ZqtORtmytmQ9thkvF1A"
-        # token =               "AAAAAAAAAAAAAAAAAAAAAEzicAEAAAAA6EZ%2BpsMWNmEaFEKsSDFqtmAU%2Bco%3DTA3yElQF4FLT2MRNd2cA70nu7xoknZ7ZqtORtmytmQ9thkvF1A"
-        # headers={'Authorization':bearer_token}
-        # client = tweepy.Client(token,wait_on_rate_limit=True)
-        # twitter_id=[]
-        # user_id=client.get_user(username=self.user_name.replace("@",'')).data.id
-        # print(user_id)
-        # token=None
-        # state=1
-        # while(1):
-        #     try:
-        #         response = client.get_liked_tweets(user_id, tweet_fields=["created_at"],max_results=100,pagination_token=token)
-        #         print(response)
-        #         if(response.data==None):
-        #             break 
-        #         for tweet in response.data:
-        #             self._progressbar.emit(-1,len(twitter_id))
-        #             if(self.tweet_id!=None):
-        #                 if (tweet.id==int(self.tweet_id)):
-        #                     state=0
-        #                     break  
-        #             twitter_id.append(tweet.id)
-        #         if state==0:break
-        #         token=response.meta['next_token']
-        #         print(token)
-        #     except:
-        #         print(response)
-        #     sleep(1)   
-        # if  twitter_id==[]:
-        #     self._finish.emit(-1)
-        #     return 
-        # self.tweet_id=str(twitter_id[0])    
 
-        # # 將ID分成多組
-        # string = [twitter_id[i:i+100] for i in range(0, len(twitter_id), 100)]
-        # match = re.search("https://pbs.twimg.com/media/(.*)(?:\.|\?.*)", self.last_url)
-        # if match:
-        #     last_url = match.group(1)
-        # else:
-        #     last_url='NONENONONOONONNONONONONONONONONONONOONONONO'
-        # state=1
-        # for i in range(0,len(string)):
-        #     ids=string[i]
-        #     twitteridstr=''
-        #     for id in ids:
-        #         if twitteridstr !='':
-        #             twitteridstr=twitteridstr+','+str(id)
-        #         else:
-        #             twitteridstr=str(id)
-        #     url=('https://api.twitter.com/2/tweets?ids={}&tweet.fields=lang&expansions=attachments.media_keys&media.fields=url'.format(twitteridstr))
-        #     print(url)
-        #     i=requests.get(url,headers=headers)
-        #     try:
-        #         media_list=i.json()['includes']['media']
-        #         for media in media_list:
-        #             try:
-        #                 #print(media['url'])
-        #                 link=media['url']
-        #                 name=link.replace("https://pbs.twimg.com/media/","").split(".")[0]
-        #                 url=link+":orig"
-        #                 download.append(url)
-        #                 if(last_url in name):
-        #                     state=0
-        #                     break
-        #             except:
-        #                 pass
-        #     except:
-        #         pass
-        #     if (state==0):
-        #         break
-        #     self._progressbar.emit(3,len(string))
-        # self.urls=len(download)
-        # self._progressbar.emit(2,self.urls)
-        # download = download[::-1]
-        
-        # if download!=[]:
-        #     with concurrent.futures.ThreadPoolExecutor(max_workers=self.download_thread_num) as self.executor:
-        #         func=partial(self.download_twitter_img,self.download_path+'/')
-        #         self.executor.map(func,download)
-        #     self.previousLink=self.last_url
-        #     self.last_url=download[0]
-        #     self.save_info()
-        #     self._finish.emit(1)
-        # else:
-        #     self._finish.emit(-1)
     
   
   
